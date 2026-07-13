5b74d5a9fc82d434542843ab87ba54738597435c
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:34:17 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -25,16 +25,16 @@ EOT
     iothub_id                               = string
     name                                    = string
     resource_group_name                     = string
-    authentication_type                     = optional(string) # Default: "keyBased"
-    batch_frequency_in_seconds              = optional(number) # Default: 300
+    authentication_type                     = optional(string)
+    batch_frequency_in_seconds              = optional(number)
     connection_string                       = optional(string)
     connection_string_key_vault_id          = optional(string)
     connection_string_key_vault_secret_name = optional(string)
-    encoding                                = optional(string) # Default: "Avro"
+    encoding                                = optional(string)
     endpoint_uri                            = optional(string)
-    file_name_format                        = optional(string) # Default: "{iothub}/{partition}/{YYYY}/{MM}/{DD}/{HH}/{mm}"
+    file_name_format                        = optional(string)
     identity_id                             = optional(string)
-    max_chunk_size_in_bytes                 = optional(number) # Default: 314572800
+    max_chunk_size_in_bytes                 = optional(number)
     subscription_id                         = optional(string)
   }))
 }
